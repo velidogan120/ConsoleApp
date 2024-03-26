c8e6581d6b6170a7808b99c25eeaab61e66e519c
Veli Dogan <velidogan120@gmail.com>
2024-03-26 03:12:47 +0300

    1

--- obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b363215277f7054568c92d8885e4e8e1d6507e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaee75ca36e632413a706f58ec78be4f8f75b462")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
